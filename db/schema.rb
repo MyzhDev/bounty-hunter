b881be34b9d459a520e83909d5f48d450b8a9b9f
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 12:40:33 +0100

    Config for Cloudinary and Active storage done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_103430) do
+ActiveRecord::Schema.define(version: 2022_02_22_113610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "contracts", force: :cascade do |t|
     t.float "rating"
@@ -39,6 +67,21 @@ ActiveRecord::Schema.define(version: 2022_02_22_103430) do
     t.index ["user_id"], name: "index_hunters_on_user_id"
   end
 
+  create_table "specialities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "speciality_hunters", force: :cascade do |t|
+    t.bigint "speciality_id", null: false
+    t.bigint "hunter_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hunter_id"], name: "index_speciality_hunters_on_hunter_id"
+    t.index ["speciality_id"], name: "index_speciality_hunters_on_speciality_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,7 +96,11 @@ ActiveRecord::Schema.define(version: 2022_02_22_103430) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contracts", "hunters"
   add_foreign_key "contracts", "users"
   add_foreign_key "hunters", "users"
+  add_foreign_key "speciality_hunters", "hunters"
+  add_foreign_key "speciality_hunters", "specialities"
 end
