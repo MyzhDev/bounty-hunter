bd767bb0b5c4cf6f75fd287e8caa1c7cacb0b895
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 18:00:18 +0100

    Created the possibility to offer a contract to a hunter

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,26 @@
 class ContractsController < ApplicationController
+  def new
+    @user = current_user
+    @contract = Contract.new
+    @hunter = Hunter.find(params[:id])
+  end
+  def create
+    @user = current_user
+    @contract = Contract.new(contract_params)
+    @hunter = Hunter.find(params[:hunter_id])
+    @contract.user = @user
+    @contract.hunter = @hunter
+
+    if @contract.save!
+      redirect_to hunter_path(@hunter)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(:title, :description)
+  end
 end
