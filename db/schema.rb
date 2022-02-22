7b9ee7c2dc1162b8910955fe9744b9801c2f81d2
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 11:39:32 +0100

    Added model Contract with hunter and user references + migration + validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_155835) do
+ActiveRecord::Schema.define(version: 2022_02_22_103430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contracts", force: :cascade do |t|
+    t.float "rating"
+    t.boolean "status"
+    t.text "description"
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.bigint "hunter_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hunter_id"], name: "index_contracts_on_hunter_id"
+    t.index ["user_id"], name: "index_contracts_on_user_id"
+  end
+
+  create_table "hunters", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "catchphrase"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hunters_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +53,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_155835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contracts", "hunters"
+  add_foreign_key "contracts", "users"
+  add_foreign_key "hunters", "users"
 end
