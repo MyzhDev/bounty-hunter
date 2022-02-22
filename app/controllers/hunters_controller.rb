74630e14af38d7f9d302286ef91aee670cf96976
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 16:31:50 +0100

    Controller for hunter

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -13,17 +13,22 @@ class HuntersController < ApplicationController
 
   def create
     @hunter = Hunter.new(hunter_params)
-    if @hunter.save
+    @hunter.user = current_user
+    if @hunter.save!
+      params[:hunter][:speciality_hunter][:speciality].each do |s|
+        if s.present?
+          SpecialityHunter.create(hunter: @hunter, speciality_id: s.to_i)
+        end
+      end
       redirect_to hunters_path
     else
       render :new
     end
   end
 
-
   private
 
   def hunter_params
-    params.require(:hunter).permit(:name, :photo, :price, :catchphrase, :description, :specialty)
+    params.require(:hunter).permit(:name, :photo, :price, :catchphrase, :description)
   end
 end
--- app/models/speciality.rb
+++ app/models/speciality.rb
@@ -1,2 +1,3 @@
 class Speciality < ApplicationRecord
+
 end
