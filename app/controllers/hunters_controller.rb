b202233cd5bbbaf4acbd8c2723ab0d732090b961
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 13:04:07 +0100

    created methods index, show, new & create for hunter controller

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -1,2 +1,29 @@
 class HuntersController < ApplicationController
+  def index
+    @hunters = Hunter.all
+  end
+
+  def show
+    @hunter = Hunter.find(params[:id])
+  end
+
+  def new
+    @hunter = Hunter.new
+  end
+
+  def create
+    @hunter = Hunter.new(hunter_params)
+    if @hunter.save
+      redirect_to hunters_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def hunter_params
+    params.require(:hunter).permit(:name, :photo, :price, :catchphrase, :description, :specialty)
+  end
 end
