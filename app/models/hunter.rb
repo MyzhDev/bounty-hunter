2e9a9000c2afd5a74c7fc130cfa087a27db1cae4
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 12:22:22 +0100

    Added specialities tags, migration and added specialty to hunter

--- app/models/hunter.rb
+++ app/models/hunter.rb
@@ -1,6 +1,7 @@
 class Hunter < ApplicationRecord
   belongs_to :user
   has_many :contracts
+  has_many :specialities, through: :speciality_hunters
   validates :name, :description, :price, presence: true
   has_one_attached :photo
 end
---
+++ test/models/speciality_hunter_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SpecialityHunterTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
