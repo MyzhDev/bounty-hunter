e463f5a000ae09cb47d167b9009e9bac66485b75
Rayane Nordine <rayanenordine@gmail.com>
2022-02-22 11:21:47 +0100

    created model hunter with a user reference + migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_155835) do
+ActiveRecord::Schema.define(version: 2022_02_22_101931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hunters", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "catchphrase"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hunters_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_02_21_155835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "hunters", "users"
 end
